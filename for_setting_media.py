ae94c598a54a0f64902340a305b03b12fe409a0b
raritto-1 <mohitmeena18803@gmail.com>
2024-09-14 22:18:05 +0530

    setting_static_media

---
+++ for_setting_media.py
@@ -0,0 +1,16 @@
+# data for set the image media file in python
+import os
+
+# Base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Media files (uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
